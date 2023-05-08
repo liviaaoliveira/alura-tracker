2dc6f7e646b1c2316a2640d3dfb72b584e566f24
Livia Oliveira <livia.deoliveira@live.com>
2023-05-08 14:43:51 -0300

    adding a json to the project

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,10 +4,13 @@ import { Store, createStore, useStore as vuexUseStore } from "vuex";
 import {
   ADICIONA_PROJETO,
   ALTERA_PROJETO,
+  DEFINIR_PROJETOS,
   EXCLUIR_PROJETO,
   NOTIFICAR,
 } from "./tipo-mutacoes";
 import { INotificacao } from "@/interfaces/INotificacao";
+import { OBTER_PROJETOS } from "./tipo-acoes";
+import http from "@/http";
 
 interface Estado {
   projetos: IProjeto[];
@@ -36,6 +39,9 @@ export const store = createStore<Estado>({
     [EXCLUIR_PROJETO](state, id: string) {
       state.projetos = state.projetos.filter((proj) => proj.id != id);
     },
+    [DEFINIR_PROJETOS](state, projetos: IProjeto[]) {
+      state.projetos = projetos;
+    },
     [NOTIFICAR](state, novaNotificacao: INotificacao) {
       novaNotificacao.id = new Date().getTime();
       state.notificacoes.push(novaNotificacao);
@@ -47,6 +53,14 @@ export const store = createStore<Estado>({
       }, 3000);
     },
     /* eslint-disable */
+  },
+
+  actions: {
+    [OBTER_PROJETOS]({ commit }) {
+      http
+        .get("projetos")
+        .then((resposta) => commit(DEFINIR_PROJETOS, resposta.data));
+    },
   },
 });
 
