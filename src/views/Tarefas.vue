ea36b44dcadb88f04de802728a632a1ed5411ae1
Livia Oliveira <livia.deoliveira@live.com>
2023-05-09 14:50:24 -0300

    refactoring tarefas file

--- src/views/Tarefas.vue
+++ src/views/Tarefas.vue
@@ -58,7 +58,7 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, ref } from "vue";
+import { computed, defineComponent, ref, watchEffect } from "vue";
 import Formulario from "../components/Formulario.vue";
 import Tarefa from "../components/Tarefa.vue";
 import Box from "../components/Box.vue";
@@ -110,14 +110,13 @@ export default defineComponent({
     store.dispatch(OBTER_PROJETOS);
 
     const filtro = ref("");
-    const tarefas = computed(() =>
-      store.state.tarefa.tarefas.filter(
-        (t) => !filtro.value || t.descricao.includes(filtro.value)
-      )
-    );
+   
+    watchEffect(() => {
+      store.dispatch(OBTER_TAREFAS, filtro.value);
+    });
 
     return {
-      tarefas,
+      tarefas: computed(() => store.state.tarefa.tarefas),
       store,
       filtro,
     };
